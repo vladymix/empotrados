0de83872c1fcde679d9fc9b4a81dc8ae73605007
Fabricio Altamirano <fabricio.altamirano>
2021-11-19 00:44:37 +0100

    Pruebas commit mac

--- p4/Ejemplo.c
+++ p4/Ejemplo.c
@@ -4,7 +4,7 @@
 #pragma config CPD = OFF, BOREN = OFF, IESO = OFF, DEBUG = OFF, FOSC = HS
 #pragma config FCMEN = OFF, MCLRE = ON, WDTE = OFF, CP = OFF, LVP = OFF
 #pragma config PWRTE = ON, BOR4V = BOR21V, WRT = OFF
-#pragma intrinsic(_delay)
+#pragma intrinsic(_delay) // Preguntar
 
 
 #define _XTAL_FREQ 20000000 // necessary for __delay_usint contador;
